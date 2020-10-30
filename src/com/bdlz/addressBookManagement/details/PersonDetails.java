8573320b8d5beaa47efb8d1a5f0738159d202544
Rachana <rachanarrachu28@gmail.com>
2020-10-30 21:49:32 +0530

    Refactored : Person details are added using personDetails

--- src/com/bdlz/addressBookManagement/details/PersonDetails.java
+++ src/com/bdlz/addressBookManagement/details/PersonDetails.java
@@ -20,3 +20,59 @@ public class PersonDetails {
 		this.zipCode = zipCode;
 	}
 }
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public String getCity() {
+	return city;
+}
+
+public void setCity(String city) {
+	this.city = city;
+}
+
+public String getState() {
+	return state;
+}
+
+public void setState(String state) {
+	this.state = state;
+}
+
+public String getMobileNum() {
+	return mobileNum;
+}
+
+public void setMobileNum(String mobileNum) {
+	this.mobileNum = mobileNum;
+}
+
+public String getZipCode() {
+	return zipCode;
+}
+
+public void setZipCode(String zipCode) {
+	this.zipCode = zipCode;
+}
+}
